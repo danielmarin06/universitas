d46be194a9d2f7324c39be33bd0277e4e164b8a9
Oscar Mesa <oscar.mesa@chiper.co>
2024-04-18 20:14:10 -0500

    cambios

---
+++ src/main/java/co/edu/poli/ces3/universitas/database/ConexionMySql.java
@@ -0,0 +1,15 @@
+package co.edu.poli.ces3.universitas.database;
+
+public class ConexionMySql {
+
+    int numbersConexions;
+    protected String nameConexion;
+    private int numbersConexion;
+    public double isAvailable;
+
+
+    public int conexion(int available){
+        return 0;
+    }
+
+}
