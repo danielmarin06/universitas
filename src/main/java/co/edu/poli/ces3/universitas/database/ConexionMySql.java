bfda4f3c644f729e9c33f10d52215ed9b7ea137f
Oscar Mesa <oscar.mesa@chiper.co>
2024-04-25 20:21:57 -0500

    conexion mysql

--- src/main/java/co/edu/poli/ces3/universitas/database/ConexionMySql.java
+++ src/main/java/co/edu/poli/ces3/universitas/database/ConexionMySql.java
@@ -1,15 +1,49 @@
 package co.edu.poli.ces3.universitas.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionMySql {
 
-    int numbersConexions;
-    protected String nameConexion;
-    private int numbersConexion;
-    public double isAvailable;
+    private int port;
+    private String host;
+    private String user;
+    private String password;
+    private String dataBaseName;
+    public static final long SERIAL = 1L;
 
-
-    public int conexion(int available){
-        return 0;
+    ConexionMySql(String host){
+        user = "root";
+        password = "";
+        this.host= host != null ? host : "127.0.0.1";
+        this.dataBaseName = "ces3-universitas";
     }
+
+
+    public void disconect(){
+    }
+    public Connection conexion(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection( "jdbc:mysql://" +host+":"+port+"/"+ dataBaseName, user, password)
+        }catch (ClassNotFoundException | SQLException exception){
+            System.out.println(exception.getMessage());
+        }
+        return null;
+    }
+
+    public static int sum(int a, int b){
+        return a + b;
+    }
+    public static void main(String[] args) {
+
+        ConexionMySql con = new ConexionMySql();
+
+        System.out.println("la suma es " + ConexionMySql.sum(3,4));
+        System.out.println(ConexionMySql.SERIAL);
+        System.out.println("Hello Conection MySQL");
+    }
+
 
 }
